fc875fec963e7acc0259330259e60c8b0254eb7c
Chanwoo Nam <chanwoo.1993n@gmail.com>
2019-08-28 17:27:17 +0900

    test telegram-chatbot

---
+++ telegram.py
@@ -0,0 +1,23 @@
+"""
+python으로 telegram message 보내기
+"""
+
+import requests
+
+base_url ='https://api.telegram.org'
+token = '985446231:AAGqbgEOD_0k5hujNfywoeaGVCDFlTUMtds'
+
+# (1) getUpdates를 통해 chat_id를 가져오자 
+url = f'{base_url}/bot{token}/getUpdates'
+
+response = requests.get(url)
+res_dict = response.json()
+print(res_dict)
+chat_id = res_dict['result'][0]['message']['chat']['id']
+
+# (2) url을 조합하여 requests로 요청 보내기 
+text = '언제 금요일'
+url = f'{base_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
+
+print(url)
+requests.get(url)
